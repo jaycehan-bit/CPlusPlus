e740d8fc470d9ce236663b25079a8a73dd8653d9
jaycehan <jaycehan@tencent.com>
2024-03-20 21:05:26 +0800

    Initial Commit

---
+++ CPlusPlus/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CPlusPlus
+//
+//  Created by jaycehan on 2024/3/20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
